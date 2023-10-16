1e5def941419dabf7ad97912541709545b28a3fa
nappelbaum <123078983+nappelbaum@users.noreply.github.com>
2023-10-16 22:43:29 +0300

    Update botForGoogle.js

--- Bot/botForGoogle.js
+++ Bot/botForGoogle.js
@@ -3,8 +3,9 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       AgafonovAS
 // @match        https://www.google.com/*
+// @match        https://www.kia.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -13,26 +14,61 @@ let input = document.getElementsByName("q")[0];
 let checkEl = document.querySelector("input[name='btnK']");
 let links = document.links;
 let keywords = ["каталог автомобилей", "продажа автомобилей", "купить автомобиль"];
-let keyword = keywords[getRand(0, keywords.length)];
+let keyword = keywords[getRandom(0, keywords.length)];
 
+//Работаем на главной странице
 if (checkEl != null) {
-    input.value = keyword;
-
-    let ev = new KeyboardEvent('keydown', {
+  let i = 0;
+  let timerId = setInterval(() => {
+    input.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      let ev = new KeyboardEvent('keydown', {
         keyCode: 13,
         bubbles: true
     });
     input.dispatchEvent(ev);
-}
-
-for (let i = 0; i < links.length; i++) {
-    let link = links[i];
-    if (link.href.includes("auto.ru")) {
-        link.click();
-        break;
     }
-}
+  }, 200)
 
-function getRand (min, max) {
+} else if (location.hostname == "www.kia.ru") {
+    //Работаем на целевом сайте
+    console.log("Мы на целевом сайте!");
+    setInterval(() => {
+      let index = getRandom(0, links.length);
+
+      if (getRandom(0, 101) >= 80) {
+        location.href = "https://www.google.com/";
+      }
+     if (links[index].href.includes("kia.ru")) {
+       links[index].click();
+      }
+    }, getRandom(2000, 5000))
+
+  } else {
+    //Работаем на странице поисковой выдачи
+    let i = 0;
+    function interval() {
+        for (let i = 0; i < links.length; i++) {
+            let link = links[i];
+            if (link.href.includes("kia.ru")) {
+                link.click();
+                break;
+            }
+        }
+
+        i++;
+        if (i <= 6) {
+            window.scrollTo(0, document.body.scrollHeight);
+            setTimeout(interval, getRandom(2500, 5000));
+        }
+        else location.href = "https://www.google.com/";
+    }
+    setTimeout(interval, getRandom(2500, 5000));
+  }
+
+
+function getRandom (min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
